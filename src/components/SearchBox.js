6ce5308c51fddf2bdbace0b1ce155afe73f9924c
Siddhesh Mangela <sidthecool007@gmail.com>
2020-08-28 16:24:06 +0530

    inputStyles separated

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,5 +1,23 @@
 import { div, input } from '@hyperapp/html';
 
+const inputStyles = `
+  bg-white
+  w-full
+  h-12
+  px-4
+  text-base
+  font-semibold
+  rounded-md
+  shadow
+  focus:shadow-lg
+  transition-shadow
+  duration-200
+  outline-none
+  placeholder-gray-500
+  text-gray-700
+  tracking-wide
+`;
+
 export default function SearchBox() {
   return div(
     {
@@ -8,8 +26,7 @@ export default function SearchBox() {
     },
     [
       input({
-        class:
-          'bg-white w-full h-12 px-4 text-base font-semibold rounded-md shadow focus:shadow-lg transition-shadow duration-200 outline-none placeholder-gray-500 text-gray-700 tracking-wide',
+        class: inputStyles,
         type: 'text',
         placeholder: '🔎 Search City',
       }),
